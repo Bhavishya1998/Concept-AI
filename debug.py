3e4ab57f824bcdff0ee283d2e5e36813fce92dbb
Abhijay Sahay <abhijaygame@gmail.com>
2019-08-25 23:00:37 +0530

    Can make first AI move random.

--- debug.py
+++ debug.py
@@ -13,3 +13,5 @@ def print_board(s: State, flip_vertical=False, flip_horizontal=False):
             if s.board[cell_y][cell_x] == EMPTY:
                 print(" ", end="|")
         print()
+
+    print()
