c611791309f065ef5c23f6a20e9477f9251c5b7e
Abhijay Sahay <abhijaygame@gmail.com>
2019-09-22 10:43:09 +0100

    Add debugging functions for future state.

--- connect4/debug.py
+++ connect4/debug.py
@@ -1,4 +1,4 @@
-from state import State, RED, YELLOW, EMPTY, BOARD_WIDTH, BOARD_HEIGHT, EMPTY, UNAVAILABLE, SINGLE, DOUBLE, ATTACK
+from state import State, RED, YELLOW, EMPTY, BOARD_WIDTH, BOARD_HEIGHT, EMPTY, UNAVAILABLE, SINGLE, DOUBLE, ATTACK, NUM_ROW_LINES, NUM_COL_LINES, NUM_DIAG_LINES_ONE_SIDE
 
 def print_board(state: State):
     """ Print the game board to console. """
@@ -21,6 +21,31 @@ def adjust_cell(cell):
 
     r, c = cell
     return 6 - r, c + 1
+
+def adjust_line(line, state=None):
+    """
+    Adjust a line to fit the format of the email thread.
+    State must passed for diagonals.
+    """
+
+    if line < NUM_ROW_LINES:
+        row, shift = divmod(line, 4)
+        adjusted_row = 6 - row
+        return ("0" * shift) + (str(adjusted_row) * 4) + ("0" * (3 - shift))
+    elif line < NUM_ROW_LINES + NUM_COL_LINES:
+        col, row = divmod(line - NUM_ROW_LINES, 3)
+        adjusted_row, _ = adjust_cell((row, col))
+        return ("0" * col) + str(adjusted_row - 3) + ("0" * (6 - col))
+    elif line < NUM_ROW_LINES + NUM_COL_LINES + NUM_DIAG_LINES_ONE_SIDE:
+        top_cell = state.line_to_cells(line)[0]
+        _, top_cell_c = top_cell
+        adjusted_cell_r, _ = adjust_cell(top_cell)
+        return ("0" * (top_cell_c - 3)) + (''.join([str(i) for i in range(adjusted_cell_r - 3, adjusted_cell_r + 1)])) + ("0" * (6 - top_cell_c))
+    else:
+        top_cell = state.line_to_cells(line)[0]
+        _, top_cell_c = top_cell
+        adjusted_cell_r, _ = adjust_cell(top_cell)
+        return ("0" * top_cell_c) + (''.join([str(i) for i in range(adjusted_cell_r, adjusted_cell_r - 4, -1)])) + ("0" * (3 - top_cell_c))
 
 def line_status_str(status):
     if status == EMPTY:
@@ -56,3 +81,19 @@ def print_count_vector(state: State):
     print("\nYELLOW")
     for k, v in state.state_vector[YELLOW].items():
         print(k, " : ", len(v))
+
+def print_future_state_table(state: State):
+    """ Print out a table of future states for both players """
+
+    red_fs = state.future_state(RED)
+    yellow_fs = state.future_state(YELLOW)
+
+    print("RED")
+    for line, val in red_fs.items():
+        status, fs = val
+        print(f"line {adjust_line(line, state)} | {line_status_str(status)} | future state {[adjust_cell(cell) for cell in fs]}")
+
+    print("\nYELLOW")
+    for line, val in yellow_fs.items():
+        status, fs = val
+        print(f"line {adjust_line(line, state)} | {line_status_str(status)} | future state {[adjust_cell(cell) for cell in fs]}")
